b6dd91c1bdd5749f28038a1e8b0ee2b253c04c8d
The Boss <pierre.mitou1@gmail.com>
2024-04-10 19:02:46 +0200

    creation classes Image et section + relation

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreImage;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreImageRequest;
+use Intervention\Image\ImageManagerStatic;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $images = Image::paginate(9);
+        
+        return view('pages.admin.photos.index', compact('images'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pages.admin.photos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreImageRequest $request)
+    {
+        $filepath = request('image');
+        
+        if(request('format') == 0)
+        {
+            $width = 2000;
+            $height = 1333;
+        }
+        else
+        {
+            $width = 1333;
+            $height = 2000;
+        }
+
+        $photo = ImageManagerStatic::make($filepath)->fit($width,$height);
+        $photoName = Str::random(10) . time() . ".webp";
+        Storage::disk('public')->put('/images/' . $photoName, $photo->encode('webp'));
+
+
+        $image = new Image();
+        $image->alt = $request->alt;
+        $image->filepath = '/images/' . $photoName;
+        $image->save();
+
+        return redirect('/admin/photos');
+
+    }
+    /**
+     * Display the specified resource.
+     */
+    public function show(Image $image)
+    {
+        //
+    }
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Image $image)
+    {
+        // Vérifier si l'image existe (dans le dossier public), si oui, supprimer.
+        if (Storage::disk('public')->exists($image->filepath))
+        {
+            Storage::disk('public')->delete($image->filepath);
+        }
+
+        $image->delete();
+
+        return redirect('/admin/photos')->with('success','RIP');
+    }
+}
